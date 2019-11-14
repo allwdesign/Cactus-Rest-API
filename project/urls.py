d01996cbbbba9b483c8d4fc9c69a25eaf3b4c2d2
Natalia <31250744+allwdesign@users.noreply.github.com>
2019-11-14 17:22:36 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -7,8 +7,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-app_name = 'core'
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls',)),
