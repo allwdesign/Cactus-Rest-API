c8ce7088727c329298d8ae0598851a9a2a56f263
Natalia <31250744+allwdesign@users.noreply.github.com>
2019-11-14 17:33:31 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import path
 from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
 from core.views import TopicViewSet, CactusViewSet
 
+app_name = 'core'
 
 router = routers.DefaultRouter()
 router.register(r'topics', TopicViewSet)
@@ -13,7 +14,7 @@ router.register(r'cacti', CactusViewSet)
 schema_view = get_swagger_view(title='Docs for CactiAPI')
 
 urlpatterns = [
-    url(r'^docs/', schema_view)
+    path('docs/', schema_view),
 ]
 
 urlpatterns += router.urls
